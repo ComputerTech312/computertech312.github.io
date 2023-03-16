ca1e6c2a34403cbe0b5f47fd695a5c591309a949
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-03-16 18:59:22 +0000

    fixed some bugs

--- scripts.js
+++ scripts.js
@@ -1,45 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Button alert functionality
-    const myButton = document.getElementById('myButton');
+document.addEventListener("DOMContentLoaded", () => {
+  // Button alert functionality
+  const myButton = document.getElementById("myButton");
 
-    myButton.addEventListener('click', function() {
-        alert('Hello, World!');
-    });
+  myButton.addEventListener("click", () => {
+    alert("Hello, World!");
+  });
 
-    // Typing effect functionality
-    const typedTextEl = document.getElementById("typedText");
-    const cursorEl = document.getElementById("cursor");
+  // Typing animation functionality
+  const typedText = document.getElementById("typedText");
+  const cursor = document.getElementById("cursor");
+  const words = ["a programmer", "a sysadmin", "a hacker"]; // Words to cycle through
+  let wordIndex = 0; // Start with the first word in the array
+  let letterIndex = 0; // Start at the beginning of the current word
 
-    const words = ["programmer", "sysadmin", "developer", "leader", "secop", "administrator", "technician"];
-    let wordIndex = 0;
-    let letterIndex = 0;
-    let isDeleting = false;
-
-    function type() {
-        let delay = isDeleting ? 100 : 200;
-
-        if (!isDeleting && letterIndex === words[wordIndex].length) {
-            delay = 1000; // Wait for 1 second before starting to delete the word
-            isDeleting = true;
-        } else if (isDeleting && letterIndex === 0) {
-            isDeleting = false;
-            if (wordIndex < words.length - 1) {
-                wordIndex++;
-            } else {
-                wordIndex = 0;
-            }
-        }
-
-        if (!isDeleting) {
-            typedTextEl.textContent += words[wordIndex][letterIndex];
-            letterIndex++;
-        } else {
-            typedTextEl.textContent = typedTextEl.textContent.slice(0, -1);
-            letterIndex--;
-        }
-
-        setTimeout(type, delay);
+  function typeLetter() {
+    const word = words[wordIndex];
+    typedText.textContent += word[letterIndex];
+    letterIndex++;
+    if (letterIndex === word.length) {
+      // Word is fully typed, wait a few seconds before starting delete
+      setTimeout(deleteLetter, 2000);
+      return;
     }
+    setTimeout(typeLetter, 150);
+  }
 
-    type();
+  function deleteLetter() {
+    const word = words[wordIndex];
+    typedText.textContent = word.slice(0, letterIndex - 1);
+    letterIndex--;
+    if (letterIndex === 0) {
+      // Word is fully deleted, wait a few more seconds before moving on to the next word
+      setTimeout(() => {
+        wordIndex++;
+        if (wordIndex === words.length) {
+          // End of array, loop back to the beginning
+          wordIndex = 0;
+        }
+        typeLetter();
+      }, 1500);
+      return;
+    }
+    setTimeout(deleteLetter, 100);
+  }
+
+  // Start the typing animation
+  typeLetter();
+
+  // Blinking cursor animation
+  setInterval(() => {
+    cursor.style.opacity = cursor.style.opacity === "0" ? "1" : "0";
+  }, 500);
 });
