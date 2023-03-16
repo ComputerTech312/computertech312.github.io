0d6ace7758326a1e60c5e38c9097807b7c6d7332
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-03-16 17:34:43 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,30 +10,51 @@ document.addEventListener('DOMContentLoaded', function() {
     const typedTextEl = document.getElementById("typedText");
     const cursorEl = document.getElementById("cursor");
 
-    const words = ["programmer", "sysadmin", "developer", "leader", "secop", "administrator", "technician"];
+    const words = ["a programmer. ", "a sysadmin. ", "a developer. ", "an administrator. " ];
     let wordIndex = 0;
     let letterIndex = 0;
     let isDeleting = false;
+    let isFirstDelete = true;
 
     function type() {
         let delay = isDeleting ? 100 : 200;
 
         if (!isDeleting && letterIndex === words[wordIndex].length) {
-            delay = 1000; // Wait for 1 second before starting to delete the word
-            isDeleting = true;
-        } else if (isDeleting && letterIndex === 0) {
+            delay = 500; // Wait for 0.5 second before starting the blink effect
+            isDeleting = "blink"; // Set isDeleting to "blink" for the blink effect
+        } else if (isDeleting === "blink") {
+            cursorEl.classList.toggle("blink");
+            setTimeout(() => {
+                cursorEl.classList.toggle("blink");
+                setTimeout(() => {
+                    cursorEl.classList.toggle("blink");
+                    setTimeout(() => {
+                        cursorEl.classList.toggle("blink");
+                        isDeleting = true; // Start deleting the word
+                        isFirstDelete = true; // Flag to delay first letter deletion
+                        type(); // Call type function after the delay
+                    }, 500);
+                }, 500);
+            }, 500);
+            return; // Exit the function to allow blinking effect to complete
+        } else if (isDeleting === true && letterIndex === 0) {
             isDeleting = false;
             if (wordIndex < words.length - 1) {
                 wordIndex++;
             } else {
                 wordIndex = 0;
             }
+            delay = 500; // Add a delay before starting to type the next word
         }
 
         if (!isDeleting) {
             typedTextEl.textContent += words[wordIndex][letterIndex];
             letterIndex++;
         } else {
+            if (isFirstDelete) {
+                delay = 2000; // Add a 2-second delay before deleting the first letter
+                isFirstDelete = false;
+            }
             typedTextEl.textContent = typedTextEl.textContent.slice(0, -1);
             letterIndex--;
         }
