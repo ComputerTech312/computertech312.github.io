7fa8f021fc31454100d37e48b373463f3d9c5d3a
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-03-16 15:22:48 +0000

    Cleaned up the code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Button alert functionality
     const myButton = document.getElementById('myButton');
 
     myButton.addEventListener('click', function() {
         alert('Hello, World!');
     });
+
+    // Typing effect functionality
+    const typedTextEl = document.getElementById("typedText");
+    const cursorEl = document.getElementById("cursor");
+
+    const words = ["programmer", "sysadmin", "developer", "leader", "secop", "administrator", "technician"];
+    let wordIndex = 0;
+    let letterIndex = 0;
+    let isDeleting = false;
+
+    function type() {
+        let delay = isDeleting ? 100 : 200;
+
+        if (!isDeleting && letterIndex === words[wordIndex].length) {
+            delay = 1000; // Wait for 1 second before starting to delete the word
+            isDeleting = true;
+        } else if (isDeleting && letterIndex === 0) {
+            isDeleting = false;
+            if (wordIndex < words.length - 1) {
+                wordIndex++;
+            } else {
+                wordIndex = 0;
+            }
+        }
+
+        if (!isDeleting) {
+            typedTextEl.textContent += words[wordIndex][letterIndex];
+            letterIndex++;
+        } else {
+            typedTextEl.textContent = typedTextEl.textContent.slice(0, -1);
+            letterIndex--;
+        }
+
+        setTimeout(type, delay);
+    }
+
+    type();
 });
